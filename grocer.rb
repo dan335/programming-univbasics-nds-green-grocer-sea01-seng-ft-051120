7989e4959dc9d3a2909e149dd5f2762de7b29116
Daniel Phillips <danphi@gmail.com>
2020-01-24 06:06:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -118,4 +118,12 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
+  
+  pp cart
+  
+  cart
 end
